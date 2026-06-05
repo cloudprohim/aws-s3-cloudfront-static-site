4924d181b80711ae1cd246ddd5f41681508abfe9
Himanshu <cloudprohim@gmail.com>
2026-06-06 11:21:05 +1200

    Initial project structure

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "bucket_name" {
+  description = "S3 Bucket Name"
+  type        = string
+}
