8a1a5f98f4e210d43d3c85f1b5e94eae5764f3ae
Himanshu <cloudprohim@gmail.com>
2026-06-06 12:06:04 +1200

    Configure S3 static website and upload website content

--- main.tf
+++ main.tf
@@ -7,3 +7,48 @@ resource "aws_s3_bucket" "website_bucket" {
     Project     = "aws-s3-cloudfront-static-site"
   }
 }
+
+resource "aws_s3_bucket_ownership_controls" "website_bucket" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "website_bucket" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_website_configuration" "website_bucket" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+resource "aws_s3_object" "index" {
+  bucket       = aws_s3_bucket.website_bucket.id
+  key          = "index.html"
+  source       = "website/index.html"
+  content_type = "text/html"
+  etag         = filemd5("website/index.html")
+}
+
+resource "aws_s3_object" "error" {
+  bucket       = aws_s3_bucket.website_bucket.id
+  key          = "error.html"
+  source       = "website/error.html"
+  content_type = "text/html"
+  etag         = filemd5("website/error.html")
+}
---
+++ outputs.tf
@@ -0,0 +1,14 @@
+output "bucket_name" {
+  description = "Name of the S3 bucket"
+  value       = aws_s3_bucket.website_bucket.bucket
+}
+
+output "bucket_arn" {
+  description = "ARN of the S3 bucket"
+  value       = aws_s3_bucket.website_bucket.arn
+}
+
+output "website_endpoint" {
+  description = "S3 static website endpoint"
+  value       = aws_s3_bucket_website_configuration.website_bucket.website_endpoint
+}
